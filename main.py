6c4ae46c859b751627c93cb384bc9d87a29ce862
AdrianNetto <adrian.netto73@gmail.com>
2025-04-04 16:56:04 -0300

    feat: validating user input

--- main.py
+++ main.py
@@ -7,4 +7,22 @@ row1 = [' ', ' ', ' ']
 row2 = [' ', ' ', ' ']
 row3 = [' ', ' ', ' ']
 
+def user_choice():
+    while True:
+        choice = input("Please enter a number (0-8): ")
+
+        if not choice.isdigit():
+            print("This is not a digit!")
+            continue
+            
+        choice = int(choice)
+
+        if choice not in range(0, 9):
+            print("Sorry, you are out of the acceptable range (0-8)")
+            continue
+            
+        return choice
+
 display(row1, row2, row3)
+
+user_choice()
